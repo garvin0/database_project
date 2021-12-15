abb396e733257e0a397f9bc1e9e10eee3fb0544f
garvin0 <40028848+garvin0@users.noreply.github.com>
2021-12-14 19:49:30 -0500

    Delete ensur_system directory

--- ensur_system/login/views.py
+++
@@ -1,8 +0,0 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-from login.models import YwryCustomer
-# Create your views here.
-
-def index(request):
-    ywrycustomer =YwryCustomer.objects.all()
-    return HttpResponse(ywrycustomer)
